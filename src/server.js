3c1a8d0e07b894b5844c0850aacdc3ffaead2a29
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2025-12-24 20:42:58 +0100

    api setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { PORT } from '../config/env.js'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Subscription Tracker API")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+export default app;
